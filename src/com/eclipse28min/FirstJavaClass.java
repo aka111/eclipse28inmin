20536e6e5175a551e3c72f6365417668ce6b6f81
Deepa <geethapriya.p.b@gmail.com>
2021-09-30 09:30:01 -0700

    First time createdhgj

---
+++ src/com/eclipse28min/FirstJavaClass.java
@@ -0,0 +1,38 @@
+package com.eclipse28min;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class FirstJavaClass {
+	private int var1;
+	public int getVar1() {
+		return var1;
+	}
+	public void setVar1(int var1) {
+		this.var1 = var1;
+	}
+	public int getVar2() {
+		return var2;
+	}
+	public void setVar2(int var2) {
+		this.var2 = var2;
+	}
+	private int var2;
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("hello");
+			int[] numbers = {};
+			ArrayList< String> rrayList = null;
+			for (int dfj : numbers) {
+				rrayList.add("dsfjh");
+				System.out.println("git done");
+			} 
+
+
+
+}
+public void myMethod() {
+int sum, radius =2;
+System.out.println("pi here "+ 3.14 * radius * radius);
+}
+}
